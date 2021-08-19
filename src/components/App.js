c05b36c266e056952beae1a944ce43a39fb94ab3
Sam Roble <smroble1996@gmail.com>
2021-08-19 12:05:04 -0600

    submission for deadline

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BotsPage from "./BotsPage";
 
+const API = "http://localhost:8002/bots"
+
 function App() {
+const [BotCollection, setBotCollection] = useState ([])
+//I am trying to do this similarly to the practice, keeps telling me 'BotCollection is undefined'
+
+useEffect (() => {
+  fetch(API).then (res => res.json())
+  .then(json => setBotCollection(json));
+}, [])
+
   return (
     <div className="App">
       <BotsPage />
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -2,15 +2,17 @@ import React from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
-function BotsPage() {
-  //start here with your code for step one
-
+function BotsPage({ BotCollection }) {
   return (
     <div>
+      {BotCollection.map((BotCollection) => (
+        <BotCollection key={BotCollection.id} BotCollection={BotCollection} />
+      ))}
       <YourBotArmy />
-      <BotCollection />
+      <BotCollection BotCollection={BotCollection} />
+    {/* I feel like I am going bananas typing BotCollection this many times */}
     </div>
-  )
+  );
 }
 
 export default BotsPage;
